946a74a3a21dae40629f1f3c47e3292fb20b356e
uthoai <uthoaimarma597@gmail.com>
2024-06-09 12:50:59 +0600

    UI

--- app/src/main/java/com/example/unitconverterjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/unitconverterjetpackcompose/MainActivity.kt
@@ -3,27 +3,48 @@ package com.example.unitconverterjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.unitconverterjetpackcompose.ui.theme.UnitConverterJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             UnitConverterJetpackComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    UnitConverter()
                 }
             }
         }
@@ -31,17 +52,138 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun UnitConverter() {
+    var inputValue by remember { mutableStateOf("") }
+    var outputValue by remember { mutableStateOf("") }
+    var inputUnit by remember { mutableStateOf("Centimeters") }
+    var outputUnit by remember { mutableStateOf("Meters") }
+    var inExpanded by remember { mutableStateOf(false) }
+    var outExpanded by remember { mutableStateOf(false) }
+    val conversionFactor = remember { mutableStateOf(0.01) }
+
+    fun convertUnits(){
+        val inputValueDouble = inputValue.toDoubleOrNull()
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        //Here all the UI element will be stack below each other
+        Text(
+            text = "Unit Converter",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.SemiBold,
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = inputValue,
+            onValueChange = {
+            inputValue = it
+            },
+            label = { Text(text = "Enter Value")}
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+        Row {
+            // Input Box
+            Box {
+                // Input Button
+                Button(onClick = { inExpanded = true }
+                ) {
+                    Text(text = "Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
+                }
+                DropdownMenu(expanded = inExpanded, onDismissRequest = { inExpanded = false }) {
+                    DropdownMenuItem(
+                        text = { Text(text = "Centimeters") },
+                        onClick = {
+                            inExpanded = false
+                            inputUnit = "Centimeters"
+                            conversionFactor.value = 0.01
+                        }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(text = "Meters") },
+                        onClick = {
+                            inExpanded = false
+                            inputUnit = "Meters"
+                            conversionFactor.value = 0.01
+                        }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(text = "Feet") },
+                        onClick = {
+                            inExpanded = false
+                            inputUnit = "Centimeters"
+                            conversionFactor.value = 0.01
+                        }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(text = "Millimeters") },
+                        onClick = {
+                            inExpanded = false
+                            inputUnit = "Millimeters"
+                            conversionFactor.value = 0.01
+                        }
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.width(16.dp))
+            // Output Box
+            Box {
+                // Output Button
+                Button(onClick = { outExpanded = true }) {
+                    Text(text = "Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
+                }
+                DropdownMenu(expanded = outExpanded, onDismissRequest = { outExpanded = false }) {
+                    DropdownMenuItem(
+                        text = { Text(text = "Centimeters") },
+                        onClick = {
+                            outExpanded = false
+                            outputUnit = "Centimeters"
+                            conversionFactor.value = 0.01
+                        }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(text = "Meters") },
+                        onClick = {
+                            outExpanded = false
+                            outputUnit = "Meters"
+                            conversionFactor.value = 0.01
+                        }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(text = "Feet") },
+                        onClick = {
+                            outExpanded = false
+                            outputUnit = "Feet"
+                            conversionFactor.value = 0.01
+                        }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(text = "Millimeters") },
+                        onClick = {
+                            outExpanded = false
+                            outputUnit = "Millimeters"
+                            conversionFactor.value = 0.01
+                        }
+                    )
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = "Result:")
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
-    UnitConverterJetpackComposeTheme {
-        Greeting("Android")
-    }
+fun UnitConverterPreview() {
+    UnitConverter()
 }
